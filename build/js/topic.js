80e59fddb9c9dbf1bc0b9624d1f3cc58a16db114
Barpotroszek <77773157+Barpotroszek@users.noreply.github.com>
2024-02-19 00:41:36 +0100

    Add built as prev iev

---
+++ build/js/topic.js
@@ -0,0 +1,6 @@
+window.onload = () => {
+  const topic = document.getElementsByClassName("topic")[0].textContent;
+    const title =  document.getElementsByTagName("title")[0] 
+    title.textContent = topic + " | " + title.textContent;
+    console.log({title, topic})
+};
