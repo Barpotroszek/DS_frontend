11a8be761999d0f068f747dce5ae0fc4bf713405
Barpotroszek <77773157+Barpotroszek@users.noreply.github.com>
2024-02-20 16:52:42 +0100

    fix: relogin after refresh

--- src/pages/hooks/AuthContext.js
+++ src/pages/hooks/AuthContext.js
@@ -1,28 +1,49 @@
-import React, { createContext, useContext, useState } from "react";
-import { Navigate, useNavigate } from "react-router-dom";
+import React, { createContext, useContext, useEffect, useState } from "react";
+import { Navigate, useLocation, useNavigate } from "react-router-dom";
 import routes from "../components/routes";
 
 const myContex = createContext(),
   // defaultUser = { name: "Bartek", token: "123456"};
-defaultUser = null;
-
-export default function AuthProvider({ children }) {
-  const fakeAuth = () =>
+  defaultUser = null,
+  KEY = "user-credentials",
+  fakeAuth = () =>
     new Promise((resolve) => {
       setTimeout(() => resolve("23456789"), 250);
     });
 
-  const [userData, setToken] = useState(defaultUser);
-  const nav= useNavigate();
+export default function AuthProvider({ children }) {
+  const [userData, setCredentials] = useState(defaultUser),
+  location = useLocation();
+
+  const validateUser = () => {
+    const d = sessionStorage.getItem(KEY);
+    try {
+      setCredentials(JSON.parse(d));
+    } catch {} 
+    finally {
+      // if(location.state?.from?.pathname ){
+        // alert("Redirect")
+        nav(location.state?.from?.pathname)
+      // }  
+  }};
+
+  useEffect(validateUser, []);
+
+  const midSetCredentials = (c) => {
+    sessionStorage.setItem(KEY, JSON.stringify(c));
+    setCredentials(c);
+  };
+
+  const nav = useNavigate();
   const handleLogin = async (e) => {
     if (e) e.preventDefault();
     const t = await fakeAuth();
-    setToken({ name: "Bartek", token: "123456", ...t });
-    nav(routes.SELLER_DASHBOARD);    
+    midSetCredentials({ name: "Bartek", token: t });
+    nav(routes.SELLER_DASHBOARD);
   };
   const handleLogout = async (e) => {
     if (e) e.preventDefault();
-    setToken(null);
+    midSetCredentials(null);
     nav(routes.CLIENT_MAIN);
   };
 
@@ -47,9 +68,12 @@ const redirectUnauthorized = () => {
 };
 
 const ProtectedPage = ({ children }) => {
-  const { userData } = useAuthContext();
-  if (!userData) return <Navigate to={routes.CLIENT_MAIN} />;
+  const { userData } = useAuthContext(),
+  loc = useLocation();
+  if (!userData){
+    return <Navigate to={routes.CLIENT_MAIN} state={{from: loc}}/>;
+  }
   return children;
 };
-const authUser = ()=> {return true}
+
 export { useAuthContext, ProtectedPage };
