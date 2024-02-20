cac01f898f2b5df2b3d14f07ac6d3abdff40419b
Barpotroszek <77773157+Barpotroszek@users.noreply.github.com>
2024-02-20 12:33:16 +0100

    fix: add missing imports

--- src/pages/NotFound.js
+++ src/pages/NotFound.js
@@ -1,3 +1,4 @@
+import React from "react"
 import "./notFound.css"
 export default function NotFound(){
     return(
--- src/pages/hooks/AuthContext.js
+++ src/pages/hooks/AuthContext.js
@@ -1,4 +1,4 @@
-import { createContext, useContext, useState } from "react";
+import React, { createContext, useContext, useState } from "react";
 import { Navigate, useNavigate } from "react-router-dom";
 import routes from "../components/routes";
 
