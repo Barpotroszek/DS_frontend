84899ef988c2f8b6d05fbd1b67c77c4b411e1139
Barpotroszek <77773157+Barpotroszek@users.noreply.github.com>
2024-02-20 16:54:40 +0100

    fix: validating if basket defined

--- src/pages/hooks/OrderContext.js
+++ src/pages/hooks/OrderContext.js
@@ -3,10 +3,14 @@ import React, { createContext, useContext, useState } from "react";
 const myContex = createContext();
 
 export default function OrderProvider({ children }) {
-  const [basket, setBasket] = useState(JSON.parse(sessionStorage.getItem("basket")).length),
-   value = {
-    basket, setBasket
-  };
+  const [basket, setBasket] = useState(0),
+    value = {
+      basket,
+      setBasket,
+    };
+  try {
+    useState(sessionStorage.getItem("basket").length);
+  } catch {}
 
   return <myContex.Provider value={value}>{children}</myContex.Provider>;
 }
