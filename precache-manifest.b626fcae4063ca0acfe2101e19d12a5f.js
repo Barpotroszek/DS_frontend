a935ec8163d07a6659c45122947e2e7a913f506d
Barpotroszek <77773157+Barpotroszek@users.noreply.github.com>
2024-02-20 12:27:00 +0100

    Updates

---
+++ precache-manifest.b626fcae4063ca0acfe2101e19d12a5f.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "34904e33fe85a52110a31dc4a28a73c4",
+    "url": "/DS_frontend/index.html"
+  },
+  {
+    "revision": "63af12347750c847d1bb",
+    "url": "/DS_frontend/static/css/main.dd774fcc.chunk.css"
+  },
+  {
+    "revision": "7b7b31949e1e35ff90d7",
+    "url": "/DS_frontend/static/js/2.21386c00.chunk.js"
+  },
+  {
+    "revision": "33c9af1c367bdbf7cda8de087498f4d5",
+    "url": "/DS_frontend/static/js/2.21386c00.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cc0cffd11f4f0058c928",
+    "url": "/DS_frontend/static/js/3.0afb56ee.chunk.js"
+  },
+  {
+    "revision": "63af12347750c847d1bb",
+    "url": "/DS_frontend/static/js/main.22942ed9.chunk.js"
+  },
+  {
+    "revision": "23ba9173b273ba193e88",
+    "url": "/DS_frontend/static/js/runtime-main.a4d0b1eb.js"
+  },
+  {
+    "revision": "df4ca9d0fbcc76af32a4cfe752efa7ae",
+    "url": "/DS_frontend/static/media/glass.df4ca9d0.png"
+  },
+  {
+    "revision": "4fcb4f47af478e330e99fcfabbcc5c31",
+    "url": "/DS_frontend/static/media/zabity_za_prawde.4fcb4f47.jpg"
+  }
+]);
